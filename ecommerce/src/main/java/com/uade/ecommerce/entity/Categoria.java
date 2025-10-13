d848098f00eadfa4020ea740833dfe79607caa06
Ezequiel-Porcel <114034219+Ezequiel-Porcel@users.noreply.github.com>
2025-10-13 17:16:51 -0300

    Create Categoria.java
    creacion de categoria

---
+++ ecommerce/src/main/java/com/uade/ecommerce/entity/Categoria.java
@@ -0,0 +1,36 @@
+package com.uade.ecommerce.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor // Necesario para JPA
+@AllArgsConstructor // Constructor con todos los campos (útil para pruebas)
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50, nullable = false, unique = true) // Añadido 'unique' para nombres de categoría
+    private String nombre;
+
+    // Relación con Producto: Una Categoría tiene muchos Productos
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Producto> productos = new ArrayList<>();
+
+}
