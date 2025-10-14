0b24a9d03741dea51edc6244f6d9fdbdf8e1e18f
somolinari <somolinari@uade.edu.ar>
2025-10-14 11:13:23 -0300

    correccion usuario controller

--- ecommerce/src/main/java/com/uade/ecommerce/controller/UsuarioController.java
+++ ecommerce/src/main/java/com/uade/ecommerce/controller/UsuarioController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import lombok.RequiredArgsConstructor; // Importamos Lombok para la inyección por constructor
 
 @RestController
